6bbf38e50bd3752d615bd03b6e95e071cc5932b9
Cloud User <centos@ip-172-31-19-82.us-east-2.compute.internal>
2021-06-28 17:15:23 +0000

    Add a SQL database

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,19 @@
-from whitenoise import WhiteNoise
+import os
 
-from flask import Flask
-from flask import render_template
+from whitenoise import WhiteNoise
 
 from app.portfolio import portfolio
 
+from flask import Flask, request, render_template
+from . import db
+
 app = Flask(__name__)
+app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
+db.init_app(app)
 
 app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/")
 
 app.register_blueprint(portfolio)
-
 
 @app.errorhandler(404)
 def not_found(error_message):
