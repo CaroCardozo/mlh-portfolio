398e2d0bd20682cafd3dd4fbdcc9ca4ae2806778
Cloud User <centos@ip-172-31-19-82.us-east-2.compute.internal>
2021-07-03 00:46:47 +0000

    Add a volume

--- check_curl.sh
+++ check_curl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #login page
-var=`curl -s --request GET 'https://caroc.duckdns.org/login/'`
+var=`curl -s --request GET 'http://caroc.duckdns.org/login/'`
 if [ "$var" = "Login Page not yet implemented" ]; then
     printf "%s\t%s\n" "Login page" "success" 
 else 
@@ -9,7 +9,7 @@ else
 fi
 
 #register page
-var=`curl -s --request GET 'https://caroc.duckdns.org/register/'`
+var=`curl -s --request GET 'http://caroc.duckdns.org/register/'`
 if [ "$var" = "Register Page not yet implemented" ]; then
     printf "%s\t%s\n" "Register page" "success" 
 else 
@@ -17,7 +17,7 @@ else
 fi
 
 #R-Nu
-var=`curl -s --request POST 'https://caroc.duckdns.org/register/'`
+var=`curl -s --request POST 'http://caroc.duckdns.org/register/'`
 if [ "$var" = "Username is required." ]; then
     printf "%s\t%s\n" "Register (no username)" "success" 
 else 
@@ -25,7 +25,7 @@ else
 fi
 
 #R-U-Np
-var=`curl -s -X POST -d "username=mario.espinoza" 'https://caroc.duckdns.org/register/'`
+var=`curl -s -X POST -d "username=mario.espinoza" 'http://caroc.duckdns.org/register/'`
 if [ "$var" = "Password is required." ]; then
     printf "%s\t%s\n" "Register (username/no password)" "success" 
 else 
@@ -33,7 +33,7 @@ else
 fi
 
 #R-U-P
-var=`curl -s -X POST -d "username=j&password=1234" 'https://caroc.duckdns.org/register/'`
+var=`curl -s -X POST -d "username=j&password=1234" 'http://caroc.duckdns.org/register/'`
 if [ "$var" = "User r created successfully" ]; then
     printf "%s\t%s\n" "Register (username&password)" "success" 
 else 
@@ -42,7 +42,7 @@ else
 fi
 
 #R-U-P-repeated
-var=`curl -s -X POST -d "username=mario.espinoza&password=1234" 'https://caroc.duckdns.org/register/'`
+var=`curl -s -X POST -d "username=mario.espinoza&password=1234" 'http://caroc.duckdns.org/register/'`
 if [ "$var" = "User mario.espinoza is already registered." ]; then
     printf "%s\t%s\n" "Register (already registered)" "success" 
 else 
@@ -51,7 +51,7 @@ else
 fi
 
 #L-Nu
-var=`curl -s --request POST 'https://caroc.duckdns.org/login/'`
+var=`curl -s --request POST 'http://caroc.duckdns.org/login/'`
 if [ "$var" = "Incorrect username." ]; then
     printf "%s\t%s\n" "Login (no username)" "success" 
 else 
@@ -59,7 +59,7 @@ else
 fi
 
 #L-U-Np
-var=`curl -s -X POST -d "username=mario.espinoza" 'https://caroc.duckdns.org/login/'`
+var=`curl -s -X POST -d "username=mario.espinoza" 'http://caroc.duckdns.org/login/'`
 if [ "$var" = "Incorrect password." ]; then
     printf "%s\t%s\n" "Login (correct username/no password)" "success" 
 else 
@@ -67,7 +67,7 @@ else
 fi
 
 #L-Wu-Np
-var=`curl -s -X POST -d "username=mario" 'https://caroc.duckdns.org/login/'`
+var=`curl -s -X POST -d "username=mario" 'http://caroc.duckdns.org/login/'`
 if [ "$var" = "Incorrect username." ]; then
     printf "%s\t%s\n" "Login (wrong username/no password)" "success" 
 else 
@@ -75,7 +75,7 @@ else
 fi
 
 #L-U-Wp
-var=`curl -s -X POST -d "username=mario.espinoza&password=1238" 'https://caroc.duckdns.org/login/'`
+var=`curl -s -X POST -d "username=mario.espinoza&password=1238" 'http://caroc.duckdns.org/login/'`
 if [ "$var" = "Incorrect password." ]; then
     printf "%s\t%s\n" "Login (username/wrong password)" "success" 
 else 
@@ -83,7 +83,7 @@ else
 fi
 
 #L-U-P
-var=`curl -s -X POST -d "username=mario.espinoza&password=1234" 'https://caroc.duckdns.org/login/'`
+var=`curl -s -X POST -d "username=mario.espinoza&password=1234" 'http://caroc.duckdns.org/login/'`
 if [ "$var" = "Login Successful" ]; then
     printf "%s\t%s\n" "Login (username/right password)" "success" 
 else 
