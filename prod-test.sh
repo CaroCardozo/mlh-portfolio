6a2dd39cdcb099a0f5066d43576d1b0f912244b0
CaroCardozo <ccardozo1234@gmail.com>
2021-07-07 18:23:12 -0500

    Second try checkout and login&reg

---
+++ prod-test.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+count=0
+#login page
+var=`curl -s --request GET 'https://caroc.duckdns.org/login/'`
+if [ "$var" != "Login Page not yet implemented" ]; then
+    count=1
+fi
+
+#register page
+var=`curl -s --request GET 'https://caroc.duckdns.org/register/'`
+if [ "$var" != "Register Page not yet implemented" ]; then
+    count=1
+fi
+
+#R-Nu
+var=`curl -s --request POST 'https://caroc.duckdns.org/register/'`
+if [ "$var" != "Username is required." ]; then
+    count=1
+fi
+
+#R-U-Np
+var=`curl -s -X POST -d "username=mario.espinoza" 'https://caroc.duckdns.org/register/'`
+if [ "$var" != "Password is required." ]; then
+    count=1
+fi
+
+#R-U-P
+var=`curl -s -X POST -d "username=ko&password=1234" 'https://caroc.duckdns.org/register/'`
+if [ "$var" != "User ko created successfully" ]; then
+    count=1
+fi
+
+#R-U-P-repeated
+var=`curl -s -X POST -d "username=mario.espinoza&password=1234" 'https://caroc.duckdns.org/register/'`
+if [ "$var" != "User mario.espinoza is already registered." ]; then
+    count=1
+fi
+
+#L-Nu
+var=`curl -s --request POST 'https://caroc.duckdns.org/login/'`
+if [ "$var" != "Incorrect username." ]; then
+    count=1
+fi
+
+#L-U-Np
+var=`curl -s -X POST -d "username=mario.espinoza" 'https://caroc.duckdns.org/login/'`
+if [ "$var" != "Incorrect password." ]; then
+    count=1
+fi
+
+#L-Wu-Np
+var=`curl -s -X POST -d "username=mario" 'https://caroc.duckdns.org/login/'`
+if [ "$var" != "Incorrect username." ]; then
+    count=1
+fi
+
+#L-U-Wp
+var=`curl -s -X POST -d "username=mario.espinoza&password=1238" 'https://caroc.duckdns.org/login/'`
+if [ "$var" != "Incorrect password." ]; then
+    count=1
+fi
+
+#L-U-P
+var=`curl -s -X POST -d "username=mario.espinoza&password=1234" 'https://caroc.duckdns.org/login/'`
+if [ "$var" != "Login Successful" ]; then
+    count=1
+fi
+
+return $count
